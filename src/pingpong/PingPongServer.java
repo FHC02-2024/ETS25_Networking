1c041016e727a6ebca7035662f448b9d8b98d3df
Daniel Zettel <daniel.zettel@lv.campus02.at>
2025-03-22 13:51:44 +0100

    server connection

--- src/pingpong/PingPongServer.java
+++ src/pingpong/PingPongServer.java
@@ -12,6 +12,7 @@ public class PingPongServer {
         try (ServerSocket serverSocket = new ServerSocket(1111)) {
             // server soll ewig laufen
             while (true) {
+                System.out.println("server wartet auf client");
                 // server soll auf verbindung warten
                 // Kanäle für lesen und schreiben öffnen
                 try (Socket client = serverSocket.accept();
@@ -22,7 +23,7 @@ public class PingPongServer {
                              new OutputStreamWriter(client.getOutputStream())
                      )
                 ) {
-
+                    System.out.println("Client hat sich verbunden");
                     // zuerst lesen
                     String input;
                     while ((input = br.readLine()) != null) {
